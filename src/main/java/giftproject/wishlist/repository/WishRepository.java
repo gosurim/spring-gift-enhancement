d35cc9f3625439ee451b86a75b477b816060c488
gosurim <su020900@gmail.com>
2025-07-17 13:36:48 +0900

    feat: 위시 리스트 조회 페이지네이션 구현

--- src/main/java/giftproject/wishlist/repository/WishRepository.java
+++ src/main/java/giftproject/wishlist/repository/WishRepository.java
@@ -3,6 +3,8 @@ package giftproject.wishlist.repository;
 import giftproject.wishlist.entity.Wish;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Repository;
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
     List<Wish> findByMemberId(Long memberId);
+
+    Page<Wish> findByMemberId(Long memberId, Pageable pageable);
 
     Optional<Wish> findByMemberIdAndProductId(Long memberId, Long productId);
 
